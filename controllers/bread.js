573c2d48365d61326d5c4dcdab2a0ab04168f223
Brandon Life <blife1119@gmail.com>
2023-07-10 20:30:23 -0400

    added ability to create new bread

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,11 +1,36 @@
 const router = require("express").Router();
 const Bread = require("../models/bread");
-
+//Render home page
 router.get("/", (req, res) => {
   res.render("index", { breads: Bread });
 });
+//Render new page
+router.get("/new", (req, res) => {
+  res.render("new");
+});
+//Render specific bread
 router.get("/:index", (req, res) => {
   const { index } = req.params;
   res.render("show", { bread: Bread[index] });
+});
+
+// router.get("/name/:name", (req, res) => {
+//   const { name } = req.params;
+//   const bread = Bread.find((b) => b.name === name);
+//   res.send(bread);
+// });
+
+router.post("/", (req, res) => {
+  let body = req.body;
+  if (body.hasGluten === "on") {
+    body.hasGluten = true;
+  } else {
+    body.hasGluten = false;
+  }
+  if (!body.image) {
+    body.image = "https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+  }
+  Bread.push(body);
+  res.redirect("/breads");
 });
 module.exports = router;
--- views/index.jsx
+++ views/index.jsx
@@ -13,6 +13,9 @@ function Index({ breads }) {
     <Default>
       <h2>Index Page</h2>
       <ul>{breadDisplay}</ul>
+      <div>
+        <a href="/breads/new"><button>Add a new bread</button></a>
+      </div>
     </Default>
   );
 }
