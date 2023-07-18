01a7f6d9edc251c218d7298dadc6844aa8967dd5
Brandon Life <blife1119@gmail.com>
2023-07-17 20:07:49 -0400

    added mongo database connection, updated get routes

--- controllers/bread.js
+++ controllers/bread.js
@@ -1,17 +1,20 @@
 const router = require("express").Router();
 const Bread = require("../models/bread");
+
 //Render home page
-router.get("/", (req, res) => {
-  res.render("index", { breads: Bread });
+router.get("/", async (req, res) => {
+  const breads = await Bread.find()
+  res.render("index", { breads });
 });
 //Render new page
 router.get("/new", (req, res) => {
   res.render("new");
 });
 //Render specific bread
-router.get("/:index", (req, res) => {
-  const { index } = req.params;
-  res.render("show", { bread: Bread[index], index});
+router.get("/:id", async (req, res) => {
+  const { id } = req.params;
+  const bread = await Bread.findById(id)
+  res.render("show", {bread});
 });
 
 //get EDIT PAGE
@@ -26,7 +29,7 @@ router.get('/:index/edit', (req, res)=>{
 //   res.send(bread);
 // });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   let body = req.body;
   if (body.hasGluten === "on") {
     body.hasGluten = true;
@@ -34,9 +37,9 @@ router.post("/", (req, res) => {
     body.hasGluten = false;
   }
   if (!body.image) {
-    body.image = "https://images.unsplash.com/photo-1534620808146-d33bb39128b2?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80";
+    body.image = undefined
   }
-  Bread.push(body);
+  await Bread.create(req.body)
   res.status(303).redirect("/breads");
 });
 router.put("/:index", (req, res) => {
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const methodOverride = require('method-override')
+const mongoose = require("mongoose")
 require("dotenv").config();
 const breadController = require("./controllers/bread");
-
 const app = express();
 
 // MIDDLEWARE
@@ -16,6 +16,10 @@ app.engine("jsx", require("express-react-views").createEngine());
 ///routes
 app.use("/breads", breadController);
 
+// db connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('DB connected'))
+    .catch(err => console.error(err));
 const PORT = process.env.PORT;
 
 app.listen(PORT, console.log(`Listening on port ${PORT}`));
